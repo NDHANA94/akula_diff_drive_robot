fd1667757dc05292364c31c0f6afc6a05b2c4b85
Nipun <nipun.dhananjaya@gmail.com>
2023-05-01 21:28:26 +0300

    added ptcloud_to_laserscan for real robot

--- akula_bringup/launch/akula_bringup.launch.py
+++ akula_bringup/launch/akula_bringup.launch.py
@@ -18,7 +18,7 @@ import xacro
 
 def generate_launch_description():
     
-    # this_pkg_path = get_package_share_directory('akula_bringup')
+    this_pkg = get_package_share_directory('akula_bringup')
     # slam_toolbox_path = get_package_share_directory('slam_toolbox')
     # akula_description_path = get_package_share_directory('akula_description')
     # velodyne_path = get_package_share_directory('velodyne')
@@ -43,9 +43,36 @@ def generate_launch_description():
         output='screen'
     )
 
+    pointcloud2scan = Node(
+        package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+        remappings=[('cloud_in',  'velodyne_points'),
+                    ('scan', '/scan2')],
+        parameters=[{
+            'target_frame': 'vlp16_scan',
+            'transform_tolerance': 0.01,
+            'min_height': -0.65,
+            'max_height': 1.0,
+            'angle_min': -3.1415927410125732,  # -M_PI
+            'angle_max': 3.1415927410125732,  # M_PI
+            'angle_increment': 0.007000000216066837,  # M_PI/360.0
+            'scan_time': 0.0, #0.3333
+            'range_min': 0.3,
+            'range_max': 200.0,
+            'use_inf': True,
+            'inf_epsilon': 1.0
+        }],
+        name='pointcloud_to_laserscan'
+    )
+
+    params_file = os.path.join(this_pkg, 'config', 'mapper_params_online_async.yaml')
+    launch_slam_toolbox = ExecuteProcess(
+        cmd=['ros2', 'launch', 'slam_toolbox', 'online_async_launch.py', 'params_file:='+params_file],
+        output='screen'
+    )
 
     return LaunchDescription([
         launch_velodyne,
         launch_slam_toolbox,
         launch_akula_description,
+        pointcloud2scan,
     ])
